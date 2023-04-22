e94f86c8ba76b7deb74f7194ea4aebbac4b4ae51
dmn-21 <106702588+dmn-21@users.noreply.github.com>
2023-04-22 20:28:22 +0300

    Remove

--- CSharp-Advanced/DefiningClasses/07.RawData/Cargo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _07.RawData
-{
-    public class Cargo
-    {
-        public string Type { get; set; }
-
-        public int Weight { get; set; }
-    }
-}
--- CSharp-Advanced/DefiningClasses/07.RawData/Engine.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace _07.RawData
-{
-    public class Engine
-    {
-        public int Speed { get; set; }
-        public int Power { get; set; }
-    }
-}
