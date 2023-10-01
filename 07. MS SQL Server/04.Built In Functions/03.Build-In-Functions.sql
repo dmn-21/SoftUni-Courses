9e59ce4911e5001697edab5c93af40c96fd4aed2
Daniel Nikolov <106702588+dmn-21@users.noreply.github.com>
2023-10-01 11:39:32 +0300

    Add 03.Build-In-Functions

---
+++ 07. MS SQL Server/04.Built In Functions/03.Build-In-Functions.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{2C5F7B25-0AFD-471F-984D-064A587593B6}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {7841C5DB-6638-43FB-B4A7-E2555BC15DA9}
+	EndGlobalSection
+EndGlobal
